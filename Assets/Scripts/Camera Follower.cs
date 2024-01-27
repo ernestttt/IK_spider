a603ef49b7a6d8e71be81cda696217285e005cb1
Errnest <yuri.golubovski091@gmail.com>
2024-01-27 16:58:36 +0300

    camera following object

--- Assets/Scripts/Camera Follower.cs
+++ Assets/Scripts/Camera Follower.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class CameraFollower : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform _target;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update(){
+        transform.position = _target.position;
     }
 }
